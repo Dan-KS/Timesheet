0e8cb25c065d1a49dbfc277fce1fa0e703a7e662
Dan-KS <dan.kylespearman@gmail.com>
2025-09-04 17:23:43 +0100

    Create database-setup_2.sql

---
+++ docs/database-setup_2.sql
@@ -0,0 +1,14 @@
+-- PART 2: Insert Sample Data (CUSTOMIZE THIS FOR YOUR TEAM)
+
+INSERT INTO Members (Name, Email) VALUES 
+('Dan Kyle Spearman_test', 'dan.kylespearman@magentarenewables.com'),
+('Daniel Perdomo_test', 'dan.kylespearman@gmail.com'),
+('Peter Mazurenko_test', 'dan.kylespearman@gmail.com'),
+('Tim Baker_test', 'dan.kylespearman@gmail.com');
+
+INSERT INTO Projects (ProjectCode, ProjectName, Billable) VALUES 
+('CLIENT01_test', 'Client Project Alpha_test', 1),
+('CLIENT02_test', 'Client Project Beta_test', 1),
+('ADMIN_test', 'Admin Tasks', 0),
+('TRAINING_test', 'Staff Training', 0),
+('MEETING_test', 'Meetings', 0);
